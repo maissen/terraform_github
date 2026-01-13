b29121c773d56950dcb30d7d76384f68f7060591
Maissen <maissen.developer500@gmail.com>
2026-01-13 14:47:03 +0100

    Clean code after importing resources

--- imported.tf
+++ imported.tf
@@ -1,8 +1,3 @@
-import {
-    to = github_repository.r1
-    id = "terraform_github"
-}
-
 resource "github_repository" "r1" {
     name = "imported_repo"
     description = "This repo is imported to Terraform and it's provisioned by it now."
