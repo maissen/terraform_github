5b11e426306ada030f32d7352b8ae9919778b073
Maissen <maissen.developer500@gmail.com>
2026-01-16 20:24:52 +0100

    Update

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
-output "repo_details" {
-  description = "some description here"
-  value       = github_repository.my_repo1.id
-}
+# output "repo_details" {
+#   description = "some description here"
+#   value       = github_repository.my_repo1.id
+# }
 
 output "new_output" {
   value = "Hello World!"
--- resources.tf
+++ resources.tf
@@ -1,32 +1,32 @@
-resource "github_repository" "my_repo1" {
-  name            = "my_terraform_lab"
-  description     = "This github repo is provisioned by Terraform!"
-  visibility      = "public"
-  has_issues      = true
-  has_discussions = true
-  has_projects    = true
-  has_wiki        = true
-  auto_init       = true
+# resource "github_repository" "my_repo1" {
+#   name            = "my_terraform_lab"
+#   description     = "This github repo is provisioned by Terraform!"
+#   visibility      = "public"
+#   has_issues      = true
+#   has_discussions = true
+#   has_projects    = true
+#   has_wiki        = true
+#   auto_init       = true
 
-  # provisioner "local-exec" {
-  #   when = create
-  #   command = "echo Repo link : ${self.html_url}"
-  #   on_failure = continue
-  # }
+#   # provisioner "local-exec" {
+#   #   when = create
+#   #   command = "echo Repo link : ${self.html_url}"
+#   #   on_failure = continue
+#   # }
 
-  lifecycle {
-    create_before_destroy = true
-  }
-}
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
 
 
-resource "github_repository" "my_repo2" {
-  name            = "new_repo"
-  description     = "This github repo is provisioned by Terraform!"
-  visibility      = "public"
-  has_issues      = true
-  has_discussions = true
-  has_projects    = true
-  has_wiki        = true
-  auto_init       = true
-}
+# resource "github_repository" "my_repo2" {
+#   name            = "new_repo"
+#   description     = "This github repo is provisioned by Terraform!"
+#   visibility      = "public"
+#   has_issues      = true
+#   has_discussions = true
+#   has_projects    = true
+#   has_wiki        = true
+#   auto_init       = true
+# }
